d1adaf03cfbd30bbcc36f6221afbc2c7038ed6e5
kylan .H <kylan.hill@gmail.com>
2025-01-07 20:51:16 -0500

    Settings page updated to handle desired input

--- app/src/main/java/com/example/airsoftcommander/screens/TacticalBombScreen.kt
+++ app/src/main/java/com/example/airsoftcommander/screens/TacticalBombScreen.kt
@@ -1,6 +1,10 @@
 package com.example.airsoftcommander.screens
 
 import android.util.Log
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.repeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -15,11 +19,17 @@ import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,6 +42,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.airsoftcommander.model.TimerViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 import kotlin.text.contains
 
@@ -48,6 +60,8 @@ class BombViewModel : ViewModel() {
     var isBombDefused by mutableStateOf(false)
     var isPenaltyActive by mutableStateOf(false)
     var isArmingActive by mutableStateOf(false)
+
+    var isPenaltyBlinking by mutableStateOf(false)
 
     fun generateValidCode() {
         validCode = ""
@@ -85,10 +99,15 @@ fun TacticalBombScreen(
     timerViewModel: TimerViewModel = viewModel(),
     bombViewModel: BombViewModel = viewModel()
 ) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+
     var diffuseCodeText by remember { mutableStateOf("") }
     var isCodeCorrect by remember { mutableStateOf(false) }
     var isCodeCorrectText by remember { mutableStateOf("") }
     var codeColor by remember { mutableStateOf(Color.Black) }
+
 
     var timeLeft = timerViewModel.timeLeft.value
     val isTimerRunning = timerViewModel.isTimerRunning.value
@@ -100,104 +119,170 @@ fun TacticalBombScreen(
     val isPenaltyActive = bombViewModel.isPenaltyActive
     val isArmingActive = bombViewModel.isArmingActive
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        if (bombViewModel.showBombSettings) {
-            BombSettings(
-                diffuseCodeLength = bombViewModel.diffuseCodeLength,
-                bombTimerText = bombViewModel.bombTimerText,
-                guessPenaltyText = bombViewModel.guessPenaltyText,
-                armingText = bombViewModel.armingText,
-                onDiffuseCodeLengthChange = { newValue -> bombViewModel.diffuseCodeLength = newValue },
-                onBombTimerChange = { newValue -> bombViewModel.bombTimerText = newValue },
-                onGuessPenaltyChange = { newValue -> bombViewModel.guessPenaltyText = newValue },
-                onArmingTimeChange = { newValue -> bombViewModel.armingText = newValue },
-                onStartBombClick = { armingTime, bombTimer, guessPenalty ->
-                    if (armingTime.isNotEmpty() && bombTimer.isNotEmpty() && guessPenalty.isNotEmpty() && bombViewModel.diffuseCodeLength.isNotEmpty()
-                        && bombViewModel.diffuseCodeLength.toInt() != 0 && bombViewModel.diffuseCodeLength.toInt() <= 15
-                    ) {
-                        bombViewModel.startBomb()
-                       // timerViewModel.resetTimer()
-                        timerViewModel.startArmingTimer(bombViewModel.armingText.toInt()) //Passing arming time to timer
-                    } else {
-                        Log.d("Validation", "All fields must be filled")
-                    }
-                }
-            )
-        } else {
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
 
-            if (isArmingActive) {
-                ArmingTimer(timeLeft.toString())
-                if (isTimerFinished){
-                    //timerViewModel.resetTimer()
-                    timerViewModel.startTimer(bombViewModel.bombTimerText.toInt()) //passing bomb timer to timer
-                    bombViewModel.isArmingActive = false
-                }
-            }
-            if (isBombArmed && !isArmingActive) {
-                if (!isTimerRunning) { //Works but gets called 2x here
-                    Log.d("Timer", "Timer not running")
-                }
+            Column(modifier = Modifier.fillMaxSize()) {
 
-                Detonation(timeLeft.toString()) //shows Detonation with timer on end of arming
+                if (bombViewModel.showBombSettings) {
+                    BombSettings(
+                        diffuseCodeLength = bombViewModel.diffuseCodeLength,
+                        bombTimerText = bombViewModel.bombTimerText,
+                        guessPenaltyText = bombViewModel.guessPenaltyText,
+                        armingText = bombViewModel.armingText,
+                        onDiffuseCodeLengthChange = { newValue ->
+                            bombViewModel.diffuseCodeLength = newValue
+                        },
+                        onBombTimerChange = { newValue -> bombViewModel.bombTimerText = newValue },
+                        onGuessPenaltyChange = { newValue ->
+                            bombViewModel.guessPenaltyText = newValue
+                        },
+                        onArmingTimeChange = { newValue -> bombViewModel.armingText = newValue },
+                        onStartBombClick = { armingTime, bombTimer, guessPenalty ->
 
-                if (isTimerFinished){
-                    bombViewModel.isBombDetonated = true
-                    if (isBombDetonated) {
-                        Text(text = "Bomb Detonated!", color = Color.Red, fontSize = 30.sp) //shows detonated text, end of timer
-
-                    }
-                    if (isBombDefused) {
-                        Text(text = "Bomb Defused!", color = Color.Green, fontSize = 30.sp) //shows defused text, end of timer
-                    }
-                }
-                Penalty(bombViewModel.guessPenaltyText, timeLeft) // shows Penalty on end of arming
-                DiffuseCode(diffuseCodeText, isCodeCorrect, bombViewModel.validCode, isCodeCorrectText) //shows DiffuseCode on end of arming
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(bottom = 40.dp),
-                    verticalArrangement = Arrangement.Bottom
-                ) {
-                    RandomKeyboard(onItemClicked = { item -> //Shows keyboard on start of arming
-                        when {
-                            item == "Go" -> {
-                                isCodeCorrect = diffuseCodeText.length == bombViewModel.validCode.length && bombViewModel.validCode.contains(
-                                    diffuseCodeText)
-                                if (isCodeCorrect) {
-                                    isCodeCorrectText = "Correct!"
-                                    codeColor = Color.Green
-                                    bombViewModel.isBombDefused = true
-                                    timerViewModel.stopTimer()
-                                } else {
-                                    isCodeCorrectText = "Incorrect!"
-                                    codeColor = Color.Red
-                                    bombViewModel.isPenaltyActive = true
-                                    timerViewModel.startPenaltyTimer(bombViewModel.guessPenaltyText.toInt()) //calls penalty timer
+                            if (armingTime.isNotEmpty() && bombTimer.isNotEmpty() && guessPenalty.isNotEmpty() && bombViewModel.diffuseCodeLength.isNotEmpty()
+                            ) {
+                                if (armingTime.toInt() > 30) {
+                                    Log.d("Validation", "Error Arming Time more than 30")
+                                    scope.launch {
+                                        snackbarHostState.showSnackbar(
+                                            message = "Error Arming Time more than 30",
+                                            duration = SnackbarDuration.Short
+                                        )
+                                    }
+                                }else{
+                                    if (bombViewModel.diffuseCodeLength.toInt() != 0 && bombViewModel.diffuseCodeLength.toInt() <= 15) {
+                                        bombViewModel.startBomb()
+                                        // timerViewModel.resetTimer()
+                                        timerViewModel.startArmingTimer(bombViewModel.armingText.toInt()) //Passing arming time to timer
+                                    }else{
+                                        Log.d("Validation", "Error Diffuse Code 1 to 15")
+                                        scope.launch {
+                                            snackbarHostState.showSnackbar(
+                                                message = "Error Diffuse Code 1 to 15",
+                                                duration = SnackbarDuration.Short
+                                            )
+                                        }
+                                    }
                                 }
-                                diffuseCodeText = ""
-                            }
-
-                            item == "R" -> { //Resets diffuse code
-                                diffuseCodeText = ""
-                                bombViewModel.isPenaltyActive = true
-                                timerViewModel.startPenaltyTimer(bombViewModel.guessPenaltyText.toInt()) //calls penalty timer
-                            }
-
-                            diffuseCodeText.length == bombViewModel.validCode.length + 1 -> {
-                                diffuseCodeText = ""
-                            }
-
-                            else -> {
-                                diffuseCodeText += item
+                            } else {
+                                Log.d("Validation", "All fields must be filled")
+                                scope.launch {
+                                    snackbarHostState.showSnackbar(
+                                        message = "Bad setting input",
+                                        duration = SnackbarDuration.Short
+                                    )
+                                }
                             }
                         }
-                        Log.d("Button", "Button clicked: $item")
-                    })
+                    )
+                } else {
+
+                    if (isArmingActive) {
+                        ArmingTimer(timeLeft.toString())
+                        if (isTimerFinished) {
+                            //timerViewModel.resetTimer()
+                            timerViewModel.startTimer(bombViewModel.bombTimerText.toInt()) //passing bomb timer to timer
+                            bombViewModel.isArmingActive = false
+                        }
+                    }
+                    if (isBombArmed && !isArmingActive) {
+                        if (!isTimerRunning) { //Works but gets called 2x here
+                            Log.d("Timer", "Timer not running")
+                        }
+
+                        Detonation(timeLeft.toString()) //shows Detonation with timer on end of arming
+
+                        if (isTimerFinished) {
+                            bombViewModel.isBombDetonated = true
+                            if (isBombDetonated) {
+                                Text(
+                                    text = "Bomb Detonated!",
+                                    color = Color.Red,
+                                    fontSize = 30.sp
+                                ) //shows detonated text, end of timer
+
+                            }
+                            if (isBombDefused) {
+                                Text(
+                                    text = "Bomb Defused!",
+                                    color = Color.Green,
+                                    fontSize = 30.sp
+                                ) //shows defused text, end of timer
+                            }
+                        }
+                        Penalty(
+                            bombViewModel.guessPenaltyText,
+                            timeLeft,
+                            bombViewModel.isPenaltyBlinking,
+                            bombViewModel
+                        ) // shows Penalty on end of arming
+                        DiffuseCode(
+                            diffuseCodeText,
+                            isCodeCorrect,
+                            bombViewModel.validCode,
+                            isCodeCorrectText
+                        ) //shows DiffuseCode on end of arming
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(bottom = 40.dp),
+                            verticalArrangement = Arrangement.Bottom
+                        ) {
+                            RandomKeyboard(onItemClicked = { item -> //Shows keyboard on start of arming
+                                when {
+                                    item == "Go" -> {
+                                        isCodeCorrect =
+                                            diffuseCodeText.length == bombViewModel.validCode.length && bombViewModel.validCode.contains(
+                                                diffuseCodeText
+                                            )
+                                        if (isCodeCorrect) {
+                                            isCodeCorrectText = "Correct!"
+                                            codeColor = Color.Green
+                                            bombViewModel.isBombDefused = true
+                                            timerViewModel.stopTimer()
+                                        } else {
+                                            isCodeCorrectText = "Incorrect!"
+                                            codeColor = Color.Red
+                                            timerViewModel.startPenaltyTimer(bombViewModel.guessPenaltyText.toInt()) //calls penalty timer
+                                            bombViewModel.isPenaltyActive = true
+                                            bombViewModel.isPenaltyBlinking = true
+
+                                        }
+                                        diffuseCodeText = ""
+                                    }
+
+                                    item == "R" -> { //Resets diffuse code
+                                        diffuseCodeText = ""
+                                        bombViewModel.isPenaltyActive = true
+                                        timerViewModel.startPenaltyTimer(bombViewModel.guessPenaltyText.toInt()) //calls penalty timer
+                                        bombViewModel.isPenaltyBlinking = true
+                                    }
+
+                                    diffuseCodeText.length == bombViewModel.validCode.length + 1 -> {
+                                        diffuseCodeText = ""
+                                    }
+
+                                    else -> {
+                                        diffuseCodeText += item
+                                    }
+                                }
+                                Log.d("Button", "Button clicked: $item")
+                            })
+                        }
+                    }
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun BombSettings(
@@ -372,13 +457,26 @@ fun Detonation(bombTimerText:String){ //After setting, card displays timer
 }
 
 @Composable
-fun Penalty(guessPenaltyText: String, timeLeft: Int) {
-    //timeLeft =- guessPenaltyText.toInt() todo fix penalty
+fun Penalty(guessPenaltyText: String, timeLeft: Int, isBlinking: Boolean, bombViewModel: BombViewModel) {
+
+    val boarderColor by animateColorAsState(
+        targetValue = if (isBlinking) Color.Red else Color.Yellow,
+        animationSpec = repeatable(
+            iterations = 3,
+            animation = tween(150),
+            repeatMode  = RepeatMode.Reverse
+        ),
+        finishedListener = {
+            bombViewModel.isPenaltyBlinking = false
+
+        }
+    )
+
     OutlinedCard(
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surfaceVariant,
         ),
-        border = BorderStroke(3.dp, Color.Yellow),
+        border = BorderStroke(3.dp, boarderColor),
         modifier = Modifier
             .fillMaxWidth()
             .padding(start = 10.dp, end = 10.dp, bottom = 4.dp),
@@ -425,12 +523,12 @@ fun DiffuseCode(diffuseCodeText: String, isCodeCorrect: Boolean, validCode: Stri
                 style = MaterialTheme.typography.bodyLarge)
 
             Text(
-                text = validCode, fontSize = 20.sp, textAlign = TextAlign.Center,
+                text = validCode, fontSize = 28.sp, textAlign = TextAlign.Center,
                 modifier = Modifier.fillMaxWidth(),
             )
 
             Text(
-                text = "My Code: $diffuseCodeText", fontSize = 20.sp, textAlign = TextAlign.Center,
+                text = "My Code: $diffuseCodeText", fontSize = 23.sp, textAlign = TextAlign.Center,
                 modifier = Modifier.fillMaxWidth(),
             )
             Text(
@@ -496,27 +594,3 @@ fun shuffleKeyboard(keyboard: List<List<String>>): List<List<String>> {
     val shuffledItems = allItems.shuffled()
     return shuffledItems.chunked(3)
 }
-
-
-
-/*
----- todo checklist <----------
-Set inputs to Timers
-1. Arming timer
-    Hide Arming card
-    Show Other cards
-2. Detonation timer
-    End Screen
-    Button to return to games page
-3. Guess penalty
-    Subtract penalty from timer
-
-Handel inputs in settings page
- 1. Max input for diffuse code 15 ---\/
-    Need value(15), doing length right now, need both ---\/
-    Need input wronging if over
-
-Penalty card flash color when wrong input
-
- */
-
